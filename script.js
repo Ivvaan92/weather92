560096d22382a5057a72eb6bf0ca470331968343
Ivvaan92 <poliivvan@gmail.com>
2026-06-04 18:48:17 +0300

    modified script and added styles

--- script.js
+++ script.js
@@ -28,26 +28,48 @@ const inOneDay = state.forecast.forecastday[1];
 const inTwoDays = state.forecast.forecastday[2];
 
 result.innerHTML = `
+<h1 id="cityName">${state.location.name}</h1>
+<div class="container">
 <div class="card">
-<h1>${state.location.name}</h1>
-<p>${state.current.condition.text}</p>
+<h2>Today (${state.forecast.forecastday[0].date})</h2>
+<p>Weather: ${state.current.condition.text}</p>
 <img src="${state.current.condition.icon}" alt="">
-<p>Temperature: ${state.current.temp_c}</p>
-<p>Wind speed: ${state.current.wind_kph}</p>
+<p>Temperature: ${state.current.temp_c}C</p>
+<p>Wind speed: ${state.current.wind_kph} kph</p>
 <p>Sunrise time: ${state.forecast.forecastday[0].astro.sunrise}</p>
 <p>Sunset time: ${state.forecast.forecastday[0].astro.sunset}</p>
 </div>
 
 <div class="card">
 <h2>Tomorrow (${inOneDay.date})</h2>
-        <p>${inOneDay.day.condition.text}</p>
+        <p>Weather: ${inOneDay.day.condition.text}</p>
         <img src="${inOneDay.day.condition.icon}" alt="">
         <p>Average temperature: ${inOneDay.day.avgtemp_c}C</p>
-        <p>Max wind speed: ${inOneDay.day.maxwind_kph}</p>
+        <p>Max wind speed: ${inOneDay.day.maxwind_kph} kph</p>
         <p>Sunrise time: ${inOneDay.astro.sunrise}</p>
         <p>Sunset time: ${inOneDay.astro.sunset}</p>
 </div>
-        `
+
+<div class="card">
+<h2>In two days (${inTwoDays.date})</h2>
+        <p>Weather: ${inTwoDays.day.condition.text}</p>
+        <img src="${inTwoDays.day.condition.icon}" alt="">
+        <p>Average temperature: ${inTwoDays.day.avgtemp_c}C</p>
+        <p>Max wind speed: ${inTwoDays.day.maxwind_kph} kph</p>
+        <p>Sunrise time: ${inTwoDays.astro.sunrise}</p>
+        <p>Sunset time: ${inTwoDays.astro.sunset}</p>
+</div> 
+</div>
+     `
+if (state.current.condition.text == 'Sunny' || state.current.condition.text == 'Partly Cloudy') {
+    document.body.style.backgroundColor = 'lemonchiffon'
+}
+if (state.current.condition.text == 'Patchy rain nearby' || state.current.condition.text == 'Moderate or heavy rain shower') {
+    document.body.style.backgroundColor = 'skyblue'
+}
+if (state.current.condition.text == 'Overcast' || state.current.condition.text == 'Mist') {
+    document.body.style.backgroundColor = 'lightgrey'
+}
 }
 
 button.addEventListener('click', (event)=>{
